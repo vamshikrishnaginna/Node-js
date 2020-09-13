d1f798a7da8f8fcb0a4ba0346f97bc8c5772e9f2
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-09-13 20:46:26 +0530

    Passing arguments in timer function

--- timers.js
+++ timers.js
@@ -1,5 +1,13 @@
-const func = () => {
-    console.log('Hello after 4 seconds');
-}
-setTimeout(func, 4*1000);
+// const func = () => {
+//     console.log('Hello after 4 seconds');
+// }
+// setTimeout(func, 4*1000);
 
+//For: func(arg1,agr2,agr3,.....)
+//We can use: setTimeOut(func, delay, arg1, agr2, agr3,...)
+
+const rocks =(who)=>{
+    console.log(who + ' rocks!');
+}
+
+setTimeout(rocks, 4*1000, 'Vamshi');
