06e1cb243b4068ec1cd3e79538c998f18f78cefb
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-09-13 20:52:56 +0530

    Challenge of timer!! (dynamic msgs)

--- timers.js
+++ timers.js
@@ -6,8 +6,21 @@
 //For: func(arg1,agr2,agr3,.....)
 //We can use: setTimeOut(func, delay, arg1, agr2, agr3,...)
 
-const rocks =(who)=>{
-    console.log(who + ' rocks!');
+// const rocks =(who)=>{
+//     console.log(who + ' rocks!');
+// }
+
+// setTimeout(rocks, 4*1000, 'Vamshi');
+
+//CHALLENGE
+//Hello after 4 seconds
+//Hello after 8 seconds
+
+const func=(secs)=>{
+    console.log('Hello after '+secs+' seconds');
+  
 }
 
-setTimeout(rocks, 4*1000, 'Vamshi');
+setTimeout(func, 4*1000, 4 );
+setTimeout(func, 8*1000, 8 );
+
